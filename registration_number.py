0075d9f413c5e43f8fd73f08aeddb8d8b5fef313
byondmac <raghav.apoorv@gmail.com>
2015-12-16 12:36:08 +0530

    using conditional statements

--- registration_number.py
+++ registration_number.py
@@ -6,17 +6,16 @@ def check_registraion_number(registraionNumber):
 		regNum1 = registraionNumber[0] + registraionNumber[1]
 		regNum2 = registraionNumber[2] + registraionNumber[3]
 		regNum4 = registraionNumber[-4:]
+		regNum3 = registraionNumber[4] if len(registraionNumber) == 9 else registraionNumber[4] + registraionNumber[5]
 		if regNum1 == "KA" and regNum4.isdigit() and (int(regNum2) >= 0 and int(regNum2) <= 99):
 			regNum[1] = regNum1
 			regNum[4] = regNum4
 			regNum[2] = regNum2
-			if len(registraionNumber) == 9:
-				regNum3 = registraionNumber[4]
-			else:
-				regNum3 = registraionNumber[4] + registraionNumber[5]
 			if regNum3.isalpha():
 				regNum[3] = regNum3
-					print "true"
+				print regNum[1], regNum[2], regNum[3], regNum[4]
+				print "true"
+				return regNum
 			else:
 				return None
 				print "false4"
