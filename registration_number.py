47ba0bd61a16dd016345e42c9c4dea14fbf445d2
byondmac <raghav.apoorv@gmail.com>
2015-12-16 12:21:48 +0530

    optimized, reduced if elif

--- registration_number.py
+++ registration_number.py
@@ -4,31 +4,22 @@ def check_registraion_number(registraionNumber):
 	regNum = {}
 	if len(registraionNumber) >= 9 and len(registraionNumber) <=10:
 		regNum1 = registraionNumber[0] + registraionNumber[1]
-		if regNum1 == "KA":
+		regNum2 = registraionNumber[2] + registraionNumber[3]
+		regNum4 = registraionNumber[-4:]
+		if regNum1 == "KA" and regNum4.isdigit() and (int(regNum2) >= 0 and int(regNum2) <= 99):
 			regNum[1] = regNum1
-			regNum2 = registraionNumber[2] + registraionNumber[3]
-			if int(regNum2) >= 0 and int(regNum2) <= 99:
-				regNum[2] = regNum2
-				if len(registraionNumber) == 9:
-					regNum3 = registraionNumber[4]
-				else:
-					regNum3 = registraionNumber[4] + registraionNumber[5]
+			regNum[4] = regNum4
+			regNum[2] = regNum2
+			if len(registraionNumber) == 9:
+				regNum3 = registraionNumber[4]
+			else:
+				regNum3 = registraionNumber[4] + registraionNumber[5]
+			if regNum3.isalpha():
 				regNum[3] = regNum3
-				if regNum3.isalpha():
-					regNum4 = registraionNumber[-4:]
-					if regNum4.isdigit():
-						regNum[4] = regNum4
-						return regNum
-						print "true"
-					else:
-						return None
-						print "false4"
-				else:
-					return None
-					print "false3"
+					print "true"
 			else:
 				return None
-				print "false2"
+				print "false4"
 		else:
 			return None
 			print "false"
