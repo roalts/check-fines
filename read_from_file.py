b7fa937fbc949f0459809aef2a55dccd0ab0227b
roalts <raghav.apoorv@gmail.com>
2015-12-16 13:14:47 +0530

    function to read reg num from a file

---
+++ read_from_file.py
@@ -0,0 +1,13 @@
+from registration_number import check_registraion_number
+from api_request import api_requests
+
+
+def read_reg_num():
+    f = open('textfile', 'r+')
+    for number in f:
+        print number
+        number = number.strip('\n')
+        reg_num = check_registraion_number(number)
+        api_requests(reg_num)
+if __name__ == "__main__":
+    read_reg_num()
