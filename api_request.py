6d958eefb1e2efe97b99e6bfaa01bec707f23010
Raghav Apoorv <raghav.apoorv@zomato.com>
2017-10-31 10:59:20 +0530

    Restructuring api_request

--- api_request.py
+++ api_request.py
@@ -3,17 +3,17 @@ import json
 
 
 def api_requests(reg_num):
-    if reg_num is None:
+    if registration_number is None:
         print "Invalid Registration Number"
     else:
-        url = "http://btpsmartphone.appspot.com/btpfines?appversioncode=34&txtNewReg1=" + reg_num[1] + "&txtNewReg2=" + \
-              reg_num[2] + "&txtNewReg3=" + reg_num[3] + "&txtNewReg4=" + reg_num[4]
+        url = "http://btpsmartphone.appspot.com/btpfines?appversioncode=34&txtNewReg1=" + registration_number[1] + "&txtNewReg2=" + \
+              registration_number[2] + "&txtNewReg3=" + registration_number[3] + "&txtNewReg4=" + registration_number[4]
         response = urllib2.urlopen(url)
         html = response.read()
         # html = json.dumps(response.read())
-        j = json.loads(html)
+        offence_details = json.loads(html)
         if len(j) > 0:
-            result = "Offence = " + j[0].get('OFFENCE') + " Amount = " + j[0].get('AMOUNT')
+            result = "Offence = " + offence_details[0].get('OFFENCE') + " Amount = " + offence_details[0].get('AMOUNT')
             print result
             return result
         else:
